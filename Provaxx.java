89f42ae6836ea4777d60f060778f24e0996fb5c0
OTHMANE <tantatantino55@gmail.com>
2025-10-04 17:51:46 +0200

    cc

--- Provaxx.java
+++ Provaxx.java
@@ -1,6 +1,6 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Prova {
+public class Provaxx {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.

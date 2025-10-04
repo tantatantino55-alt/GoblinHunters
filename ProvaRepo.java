1c153d436454346e9b43dd2ff3ef5f8255b3f9fa
OTHMANE <tantatantino55@gmail.com>
2025-10-04 18:42:40 +0200

    cc

--- ProvaRepo.java
+++
@@ -1,7 +0,0 @@
-public class ProvaRepo {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-    }
-}
--- Test.java
+++
@@ -1,8 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
-    }
-}
-
